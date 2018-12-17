a950982932f1336c8e991282610eeb50d31a051d
Alex Darby <alex.darby@w2globaldata.com>
2018-12-17 15:55:48 +0000

    Fixed variable formatting issue

--- modules/record/variables.tf
+++ modules/record/variables.tf
@@ -1,15 +1,15 @@
-  variable "cloudflare_zone"{
+  variable "cloudflare_zone" {
       description = "(Required) The DNS zone to add the record to."
   }
-  variable "name"{
+  variable "name" {
       description = "(Required) The name of the record."
   }
-  variable "type"{
+  variable "type" {
       description = "(Required) The type of the record."
   }
-  variable "value"{
+  variable "value" {
       description = "(Optional) The (string) value of the record. Either this or data must be specified"
   }
-  variable "ttl"{
+  variable "ttl" {
       description = "(Optional) The TTL of the record (automatic: '1')"
   }
